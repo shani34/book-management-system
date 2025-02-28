310ef020ae108d5dacf740ae9af5f287ea82427e
shani kumar <shanikumar@shanis-MacBook-Air.local>
2025-03-01 00:36:01 +0530

    add secret file

--- pkg/kafka/kafka.go
+++ pkg/kafka/kafka.go
@@ -16,18 +16,18 @@ func InitKafkaProducer() {
 
     keypair, err := tls.LoadX509KeyPair("service.cert", "service.key")
     if err != nil {
-        log.Fatalf("Failed to load access key and/or access certificate: %s", err)
+        log.Printf("Failed to load access key and/or access certificate: %s", err)
     }
 
     caCert, err := ioutil.ReadFile("ca.pem")
     if err != nil {
-        log.Fatalf("Failed to read CA certificate file: %s", err)
+        log.Printf("Failed to read CA certificate file: %s", err)
     }
 
     caCertPool := x509.NewCertPool()
     ok := caCertPool.AppendCertsFromPEM(caCert)
     if !ok {
-        log.Fatalf("Failed to parse CA certificate file: %s", err)
+        log.Printf("Failed to parse CA certificate file: %s", err)
     }
 
     dialer := &kafka.Dialer{
